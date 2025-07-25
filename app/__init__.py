95107ce27bcf5b7cff619c940e19ac51c39e92df
ekagra1602 <egupta3@asu.edu>
2025-07-24 23:26:19 -0700

    greeting function added

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
- 
+import random
+
+def get_random_greeting():
+    """Return a random greeting from a predefined list."""
+    greetings = [
+        "Hello!",
+        "Hi there!",
+        "Greetings!",
+        "Howdy!",
+        "Hey!",
+        "Welcome!"
+    ]
+    return random.choice(greetings) 
